5e60ac25a14af0b77fa20ac8eab4235c66271063
Mario <mdavila0482@gmail.com>
2017-03-27 21:49:27 -0600

    cuatrocommit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'experiments/page1'
+
+  get 'experiments/page2'
+
+  get 'experiments/page3'
+
   get 'pages/batman'
-
   get 'pages/superman'
-
   get 'pages/batman_vs_superman'
-
   root 'pages#batman_vs_superman'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
